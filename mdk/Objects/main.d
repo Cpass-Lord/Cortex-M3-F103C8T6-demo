88b9fd3717ec95c839295d34135a54fdb541aefd
Kazumi3 <2586271547@qq.com>
2025-07-05 16:36:40 +0800

    project demo

---
+++ mdk/Objects/main.d
@@ -0,0 +1,4 @@
+.\objects\main.o: ..\app\main.c
+.\objects\main.o: D:\Keil5\ARM\ARMCC\Bin\..\include\stdio.h
+.\objects\main.o: D:\Keil5\ARM\ARMCC\Bin\..\include\stdbool.h
+.\objects\main.o: D:\Keil5\ARM\ARMCC\Bin\..\include\stdint.h
