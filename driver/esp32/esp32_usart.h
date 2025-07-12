11dda110e9f3ffaf3621999183621e6fe16102ed
Kazumi3 <2586271547@qq.com>
2025-07-12 11:54:58 +0800

    ESP32_AT

--- driver/esp32/esp32_usart.h
+++ driver/esp32/esp32_usart.h
@@ -11,8 +11,8 @@ void esp_usart_send_data_async(uint8_t *data, uint16_t len);
 void esp_usart_send_data(uint8_t *data, uint16_t len);
 void esp_usart_send_string(const char *string);
 void esp_usart_receive_register(esp_usart_receivecallback_t callback);
-void USART1_IRQHandler();
+void USART2_IRQHandler();
 void esp_dma_sendcomplete_register(esp_dma_sendcompletecallback_t callback);
-void DMA1_Channel4_IRQHandler();
+void DMA1_Channel7_IRQHandler();
 
 #endif // !
